c01790850c4bc7f56f849cf09586bbd2c7f1cbe6
chris <cmc3c@uw.edu>
2013-02-01 16:18:30 -0500

    Added Cosine test

--- hw4/src/test/java/ling572/CosineDistanceMetricTest.java
+++ hw4/src/test/java/ling572/CosineDistanceMetricTest.java
@@ -9,6 +9,8 @@ public class CosineDistanceMetricTest {
 	static Instance test2Instance;
 	static Instance test3Instance;
 	static Instance test4Instance;
+	static Instance test5Instance;
+	static Instance test6Instance;
 	
 	{
 		testInstance = new Instance("test");
@@ -30,10 +32,23 @@ public class CosineDistanceMetricTest {
 		test3Instance.addFeature("test4", 5090000);
 		
 		test4Instance = new Instance("test4");
-		test4Instance.addFeature("test1", -19);
-		test4Instance.addFeature("test2", -243);
-		test4Instance.addFeature("test3", -36);
-		test4Instance.addFeature("test4", -504);
+		test4Instance.addFeature("test1", 19);
+		test4Instance.addFeature("test2", 243);
+		test4Instance.addFeature("test3", 36);
+		test4Instance.addFeature("test4", 504);
+		
+		test5Instance = new Instance("test5");
+		test5Instance.addFeature("test1", 12);
+		test5Instance.addFeature("test2", 16);
+		test5Instance.addFeature("test3", 17);
+		
+		test6Instance = new Instance("test6");
+		test6Instance.addFeature("test1",16);
+		test6Instance.addFeature("test2", 76);
+		test6Instance.addFeature("test3", 27);
+		test6Instance.addFeature("test4", 9);
+		test6Instance.addFeature("test5", 88);
+				
 	}
 		
 	@Test
@@ -45,4 +60,14 @@ public class CosineDistanceMetricTest {
 		assertTrue(tester.distance(testInstance,test2Instance) < tester.distance(testInstance, test3Instance));
 		assertEquals("Result", tester.distance(testInstance, test2Instance), -tester.distance(testInstance, test4Instance), 0000001); 
 	}
+	
+	@Test
+	public void testNonIsolengthVectors() {
+		CosineDistanceMetric tester = new CosineDistanceMetric();
+		// |lhs| > |rhs|
+		assertEquals("Result", 0.677254, tester.distance(test4Instance, test5Instance), .000001);
+		// |lhs| < |rhs|
+		assertEquals("Result", 0.677254, tester.distance(test5Instance, test4Instance), .000001);
+	}
+	
 }
